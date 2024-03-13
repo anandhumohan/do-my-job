e05881aeeb0cc64fd6ea6befcad4487806bf4bfa
anandhumohan <anandhumohan5@gmail.com>
2024-03-13 05:29:23 +0000

    executable units added

--- src/main/java/com/example/domyjob/repository/InvoiceRepository.java
+++ src/main/java/com/example/domyjob/repository/InvoiceRepository.java
@@ -3,9 +3,11 @@ package com.example.domyjob.repository;
 import com.example.domyjob.model.Invoice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface InvoiceRepository extends JpaRepository<Invoice, Long>{
     @Query("SELECT i FROM Invoice i WHERE i.status = 'Pending' AND i.id IN " +
             "(SELECT p.invoiceId FROM Payment p GROUP BY p.invoiceId HAVING SUM(p.amount) >= " +
--- src/main/java/com/example/domyjob/repository/JobChainRepository.java
+++ src/main/java/com/example/domyjob/repository/JobChainRepository.java
@@ -1,14 +1,13 @@
 package com.example.domyjob.repository;
 
 import com.example.domyjob.model.JobChain;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
-import java.time.ZonedDateTime;
-import java.util.List;
 
+@Repository
 public interface JobChainRepository extends CrudRepository<JobChain, Long> {
 
     @Query("SELECT j FROM JobChain j WHERE j.scheduleTime > :now ORDER BY j.scheduleTime ASC")
